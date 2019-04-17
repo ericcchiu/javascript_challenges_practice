dc4c4648918a81ca698bcefdf257b719d1f5531b
ericcchiu <eric.chn.chiu@gmail.com>
2019-04-17 07:10:41 -0500

    (FEATURE) Add recursion solution to searching for an object in an array

--- 3. Searching for an object in an array.js
+++ 3. Searching for an object in an array.js
@@ -8,11 +8,18 @@ DO use a for loop.
 TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
 */
 /** For Loop **/
-const findPerson = (string, array) => {
-  for (let person of array) {
-    if (person.name === string) return person.hobby; 
-  }
-  return false
+// const findPerson = (string, array) => {
+//   for (let person of array) {
+//     if (person.name === string) return person.hobby; 
+//   }
+//   return false
+// }
+
+/** Recursion **/
+const findPerson = (name, people) => {
+    if (people[0] === undefined) return false; 
+    if (people[0].name === name) return people[0].hobby; 
+    return findPerson(name, people.slice(1));
 }
 
 const people = [ { "name": "Alice", "hobby": "programming" }, { "name": "Bob", "hobby": "gaming" } ];
