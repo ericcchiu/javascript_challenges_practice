fc8b160b8cca4a9852c134150904013379fc9b0d
ericcchiu <eric.chn.chiu@gmail.com>
2019-04-23 07:20:50 -0500

    (FEATURE) Add solution to exercise. Need to fix bug on recursion

---
+++ 6. Reducing an array of stings.js
@@ -0,0 +1,31 @@
+/*Given an array of strings, return all strings concatenated together.
+* DO NOT use strings.reduce() or strings.join('')
+* 
+* DO use a for loop.
+* 
+* TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+* 
+*/
+
+/** For Loop **/
+// const concatStrings = (strings, str = '') => {
+//     for (let char of strings) {
+//         str += char;
+//     }
+//     return str;
+// };
+
+/*** Recursion ***/
+const concatStrings = (strings, str ='') => { 
+   
+    if (strings.length === 0) return ""; 
+  
+    str += strings[0];
+  
+    return concatStrings(strings.slice(1), str);
+  };
+
+const strings = ['a', 'b', 'c', 'd']; 
+const empty = []; 
+console.log(concatStrings(strings)); 
+console.log(concatStrings(empty)); 
