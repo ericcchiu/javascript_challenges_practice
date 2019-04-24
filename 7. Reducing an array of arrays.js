150439ef21fee445c55e1bb4b008d10fe0208d6d
ericcchiu <eric.chn.chiu@gmail.com>
2019-04-24 15:42:07 -0500

    (FEATURE) Add ES6 spread operator solution.

--- 7. Reducing an array of arrays.js
+++ 7. Reducing an array of arrays.js
@@ -7,21 +7,25 @@
 * TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
 */
 
-const concatArrays = (arrays, flatArr = []) => {
-    // const flatArr = []
-    for (let i = 0; i < arrays.length; i+=1) {
-      for (let j = 0; j < arrays[i].length;j+=1) {
-        flatArr.push(arrays[i][j])
-      }
-    }
-    return flatArr;
-  }
-  
-  
-  const arr1 = [ [ 10, 20 ], [ 30 ], [ ] ];
-  const arr2 = [ [ 22 ], [ 33, [ 44 ] ], [ 55 ] ]; 
-  const arr3 = [];
-  
-  console.log(concatArrays(arr1));
-  console.log(concatArrays(arr2)); 
-  console.log(concatArrays(arr3)); 
+/**  For Loop **/
+// const concatArrays = (arrays, flatArr = []) => {
+//   // const flatArr = []
+//   for (let i = 0; i < arrays.length; i += 1) {
+//     for (let j = 0; j < arrays[i].length; j += 1) {
+//       flatArr.push(arrays[i][j])
+//     }
+//   }
+//   return flatArr;
+// }
+
+/** ES6 **/
+const concatArrays = (arrays) => [].concat(...arrays);
+
+
+const arr1 = [[10, 20], [30], []];
+const arr2 = [[22], [33, [44]], [55]];
+const arr3 = [];
+
+console.log(concatArrays(arr1));
+console.log(concatArrays(arr2));
+console.log(concatArrays(arr3)); 
